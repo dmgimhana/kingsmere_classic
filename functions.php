ae398827823d6a82b2d5d2bd178051c4d7a5cfb0
dmgimhana <dmgimhana@gmail.com>
2025-05-22 10:41:37 +0100

    Added custom post types, but commented the code, because we are using mu-plugins folder for that

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 
-function university_files(){
+function university_files()
+{
     wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     wp_enqueue_style("custom-google-fonts", '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style("font-awesome", '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
@@ -8,9 +9,10 @@ function university_files(){
     wp_enqueue_style('university_extra_styles', get_theme_file_uri('/build/index.css'));
 }
 
-add_action('wp_enqueue_scripts','university_files');
+add_action('wp_enqueue_scripts', 'university_files');
 
-function university_features(){
+function university_features()
+{
     // Experimentational dynamic menu code
     // register_nav_menu('headerMenuLocation', 'Header Menu Location');
     // register_nav_menu('footerLocationOne', 'Footer Location One');
@@ -18,4 +20,18 @@ function university_features(){
     add_theme_support('title-tag');
 }
 
-add_action('after_setup_theme','university_features');
+add_action('after_setup_theme', 'university_features');
+
+// function university_post_types()
+// {
+//     register_post_type('event', array(
+//         'public' => true,
+//         'labels' => array(
+//             'name' => 'Events'
+//         ),
+//         'menu_icon' => 'dashicons-calendar'
+//     ));
+// }
+
+
+// add_action('init', 'university_post_types');
