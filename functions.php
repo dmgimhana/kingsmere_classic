2aecf8248b7e09257cc2a5f2e615128905ee73b0
dmgimhana <dmgimhana@gmail.com>
2025-06-06 13:07:53 +0100

    featured-image admin step 2 and added new image size for the professor portrait in functions.php

--- functions.php
+++ functions.php
@@ -19,7 +19,10 @@ function university_features()
     // register_nav_menu('footerLocationTwo', 'Footer Location Two');
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+    add_image_size('professorLandscape', 400, 260, true);
+    add_image_size('professorPortrait', 480, 650, true);
 }
+
 
 add_action('after_setup_theme', 'university_features');
 
@@ -116,3 +119,5 @@ add_action('pre_get_posts', 'university_adjust_queries');
 // 1. Add this code segment  -> add_theme_support('post-thumbnails'); to the university_features function in functions.php
 // 2. Navigate to mu-plugins folder and add support to thumbnail
 // 3. Add a featured image to a professor using admin dashboard
+// 4. Modify single-professor.php to show the featured image
+// 5. Add a new image size for the professor portrait in functions.php
