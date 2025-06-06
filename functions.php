207bcfb9129ca510df9a8a004b932241c5cc27ca
dmgimhana <dmgimhana@gmail.com>
2025-06-06 13:43:44 +0100

    featured image admin step 6

--- functions.php
+++ functions.php
@@ -125,3 +125,4 @@ add_action('pre_get_posts', 'university_adjust_queries');
 // 7. Regenerate the thumbnails for the existing images by navigating to Tools -> Regenerate Thumbnails
 // 8. Modify single-professor.php to use the new image size i.e the_post_thumbnail('professorPortrait');
 // 9. Modify single-program.php to use the new image size i.e the_post_thumbnail_url('professorLandscape');
+// 10. If you don't want center based cropping, use a plugin called manuel image crop by tomasz and change the sizes of professorLandscape or professorPortrait if you want to
